8a3a550e275c120dd12016c9f16a55f507a407c8
purduecorona-jpg <purduecorona@gmail.com>
2026-01-18 06:06:27 +0100

    Create User.js

---
+++ User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true, trim: true },
+  email: { type: String, required: true, unique: true, trim: true },
+  password: { type: String, required: true },
+  age: { type: Number, required: true },
+  role: { type: String, enum: ["user", "admin"], default: "user" },
+  subscription: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Subscription",
+    default: null
+  },
+  createdAt: { type: Date, default: Date.now }
+});
+
+// Hash password before save
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
